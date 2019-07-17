ce0095c67e4a05cf684a1d73499662c3cbc45784
Prerana Mitta <b2302970@ben.edu>
2019-07-16 22:31:58 -0500

    autoencoder from website with multiple numbers

--- file.py
+++ file.py
@@ -1,2 +1,141 @@
-#placeholder
-#testing ability to push
+
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.datasets import mnist
+from keras.layers import Input, Dense
+from keras.models import Model
+
+#%matplotlib inline
+
+(X_train, _), (X_test, _) = mnist.load_data()
+X_train = X_train.astype('float32')/255
+X_test = X_test.astype('float32')/255
+X_train = X_train.reshape(len(X_train), np.prod(X_train.shape[1:]))
+X_test = X_test.reshape(len(X_test), np.prod(X_test.shape[1:]))
+print(X_train.shape)
+print(X_test.shape)
+
+input_img= Input(shape=(784,))
+
+encoded = Dense(units=128, activation='relu')(input_img)
+encoded = Dense(units=64, activation='relu')(encoded)
+encoded = Dense(units=32, activation='relu')(encoded)
+decoded = Dense(units=64, activation='relu')(encoded)
+decoded = Dense(units=128, activation='relu')(decoded)
+decoded = Dense(units=784, activation='sigmoid')(decoded)
+
+autoencoder=Model(input_img, decoded)
+
+encoder = Model(input_img, encoded)
+
+autoencoder.summary()
+
+encoder.summary()
+
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+autoencoder.fit(X_train, X_train,
+                epochs=50,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(X_test, X_test))
+
+encoded_imgs = encoder.predict(X_test)
+predicted = autoencoder.predict(X_test)
+
+plt.figure(figsize=(40, 4))
+for i in range(10):
+    # display original images
+    ax = plt.subplot(3, 20, i + 1)
+    plt.imshow(X_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display encoded images
+    ax = plt.subplot(3, 20, i + 1 + 20)
+    plt.imshow(encoded_imgs[i].reshape(8, 4))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    # display reconstructed images
+    ax = plt.subplot(3, 20, 2 * 20 + i + 1)
+    plt.imshow(predicted[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+plt.show()
+
+#loading only images and not their labels
+(X_train, _), (X_test, _) = mnist.load_data()
+
+X_train = X_train.astype('float32')/255
+X_test = X_test.astype('float32')/255
+
+X_train = X_train.reshape(len(X_train), np.prod(X_train.shape[1:]))
+X_test = X_test.reshape(len(X_test), np.prod(X_test.shape[1:]))
+
+X_train_noisy = X_train + np.random.normal(loc=0.0, scale=0.5, size=X_train.shape)
+X_train_noisy = np.clip(X_train_noisy, 0., 1.)
+X_test_noisy = X_test + np.random.normal(loc=0.0, scale=0.5, size=X_test.shape)
+X_test_noisy = np.clip(X_test_noisy, 0., 1.)
+print(X_train_noisy.shape)
+print(X_test_noisy.shape)
+
+# Input image
+input_img = Input(shape=(784,))
+# encoded and decoded layer for the autoencoder
+encoded = Dense(units=128, activation='relu')(input_img)
+encoded = Dense(units=64, activation='relu')(encoded)
+encoded = Dense(units=32, activation='relu')(encoded)
+decoded = Dense(units=64, activation='relu')(encoded)
+decoded = Dense(units=128, activation='relu')(decoded)
+decoded = Dense(units=784, activation='sigmoid')(decoded)
+# Building autoencoder
+autoencoder = Model(input_img, decoded)
+# extracting encoder
+encoder = Model(input_img, encoded)
+# compiling the autoencoder
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy', metrics=['accuracy'])
+# Fitting the noise trained data to the autoencoder
+autoencoder.fit(X_train_noisy, X_train_noisy,
+                epochs=100,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(X_test_noisy, X_test_noisy))
+# reconstructing the image from autoencoder and encoder
+encoded_imgs = encoder.predict(X_test_noisy)
+predicted = autoencoder.predict(X_test_noisy)
+# plotting the noised image, encoded image and the reconstructed image
+plt.figure(figsize=(40, 4))
+for i in range(10):
+    # display original images
+
+    ax = plt.subplot(4, 20, i + 1)
+    plt.imshow(X_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display noised images
+    ax = plt.subplot(4, 20, i + 1 + 20)
+    plt.imshow(X_test_noisy[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    # display encoded images
+    ax = plt.subplot(4, 20, 2 * 20 + i + 1)
+    plt.imshow(encoded_imgs[i].reshape(8, 4))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    # display reconstruction images
+    ax = plt.subplot(4, 20, 3 * 20 + i + 1)
+    plt.imshow(predicted[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+plt.show()
+
