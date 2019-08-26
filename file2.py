5c5086840016590c3271032db62e5c08236fbc29
Prerana Mitta <b2302970@ben.edu>
2019-08-25 20:38:49 -0500

    file2.py with fashion images

--- file2.py
+++ file2.py
@@ -9,12 +9,21 @@ from keras.models import Model
 #%matplotlib inline
 
 (X_train, _), (X_test, _) = mnist.load_data()
+(X_train2, _), (X_test2, _) = fashion_mnist.load_data()
+
 X_train = X_train.astype('float32')/255
 X_test = X_test.astype('float32')/255
 X_train = X_train.reshape(len(X_train), np.prod(X_train.shape[1:]))
 X_test = X_test.reshape(len(X_test), np.prod(X_test.shape[1:]))
 print(X_train.shape)
 print(X_test.shape)
+
+X_train2 = X_train2.astype('float32')/255
+X_test2 = X_test2.astype('float32')/255
+X_train2 = X_train2.reshape(len(X_train2), np.prod(X_train2.shape[1:]))
+X_test2 = X_test2.reshape(len(X_test2), np.prod(X_test2.shape[1:]))
+print(X_train2.shape)
+print(X_test2.shape)
 
 input_img= Input(shape=(784,))
 
@@ -35,14 +44,14 @@ encoder.summary()
 
 autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-autoencoder.fit(X_train, X_train,
+autoencoder.fit(X_train2, X_train2,
                 epochs=50,
                 batch_size=256,
                 shuffle=True,
-                validation_data=(X_test, X_test))
+                validation_data=(X_test2, X_test2))
 
 encoded_imgs = encoder.predict(X_test)
-predicted = autoencoder.predict(X_test)
+predicted = autoencoder.predict(X_test2)
 
 plt.figure(figsize=(40, 4))
 for i in range(10):
@@ -69,6 +78,8 @@ for i in range(10):
 plt.show()
 
 #loading only images and not their labels
+
+#number data:
 (X_train, _), (X_test, _) = mnist.load_data()
 
 X_train = X_train.astype('float32')/255
@@ -83,6 +94,22 @@ X_test_noisy = X_test + np.random.normal(loc=0.0, scale=0.5, size=X_test.shape)
 X_test_noisy = np.clip(X_test_noisy, 0., 1.)
 print(X_train_noisy.shape)
 print(X_test_noisy.shape)
+
+#fashion data:
+(X_train2, _), (X_test2, _) = fashion_mnist.load_data()
+
+X_train2 = X_train2.astype('float32')/255
+X_test2 = X_test2.astype('float32')/255
+
+X_train2 = X_train2.reshape(len(X_train), np.prod(X_train2.shape[1:]))
+X_test2 = X_test2.reshape(len(X_test), np.prod(X_test2.shape[1:]))
+
+X_train_noisy2 = X_train2 + np.random.normal(loc=0.0, scale=0.5, size=X_train2.shape)
+X_train_noisy2 = np.clip(X_train_noisy2, 0., 1.)
+X_test_noisy2 = X_test2 + np.random.normal(loc=0.0, scale=0.5, size=X_test2.shape)
+X_test_noisy2 = np.clip(X_test_noisy2, 0., 1.)
+print(X_train_noisy2.shape)
+print(X_test_noisy2.shape)
 
 # Input image
 input_img = Input(shape=(784,))
@@ -100,14 +127,14 @@ encoder = Model(input_img, encoded)
 # compiling the autoencoder
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy', metrics=['accuracy'])
 # Fitting the noise trained data to the autoencoder
-autoencoder.fit(X_train_noisy, X_train_noisy,
+autoencoder.fit(X_train_noisy2, X_train_noisy2,
                 epochs=100,
                 batch_size=256,
                 shuffle=True,
                 validation_data=(X_test_noisy, X_test_noisy))
 # reconstructing the image from autoencoder and encoder
 encoded_imgs = encoder.predict(X_test_noisy)
-predicted = autoencoder.predict(X_test_noisy)
+predicted = autoencoder.predict(X_test_noisy2)
 # plotting the noised image, encoded image and the reconstructed image
 plt.figure(figsize=(40, 4))
 for i in range(10):
